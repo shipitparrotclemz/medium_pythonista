4b37031a9bfc73d452ea88ecda80f11f61f7dcfc
Clement <clement@leadiq.com>
2022-11-30 17:54:32 +0800

    add bool

---
+++ part_one/bool.py
@@ -0,0 +1,16 @@
+parrot_is_overweight: bool = True
+print(parrot_is_overweight)
+
+parrot_weight_kg: float = 10.0
+
+more_than_10: bool = parrot_weight_kg > 10.0
+print(more_than_10)
+
+more_or_equal_to_10: bool = parrot_weight_kg >= 10.0
+print(more_or_equal_to_10)
+
+less_than_10: bool = parrot_weight_kg < 10.0
+print(less_than_10)
+
+less_or_equal_to_10: bool = parrot_weight_kg <= 10.0
+print(less_or_equal_to_10)
