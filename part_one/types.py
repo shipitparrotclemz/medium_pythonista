e6e9ca10a93ecb840b4dba20688cbe511abcd531
Clement <clement@leadiq.com>
2022-11-30 17:09:50 +0800

    add docstring for tasty seeds

--- part_one/types.py
+++ part_one/types.py
@@ -1,1 +1,4 @@
+# define an `int` variable named `tasty_seeds`. It contains an integer.
+# the `:int` to the right of the variable name
+# is an optional type-hint, telling us that the variable contains an int
 tasty_seeds: int = 10
