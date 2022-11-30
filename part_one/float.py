0ea76b5b0d99501b31f3b889b199616926afd535
Clement <clement@leadiq.com>
2022-11-30 17:43:41 +0800

    add float

---
+++ part_one/float.py
@@ -0,0 +1,16 @@
+parrot_weight: float = 10.0
+
+print(parrot_weight)
+
+parrot_weight: float = parrot_weight + 10.0
+
+print(parrot_weight)
+
+parrot_weight -= 10
+
+print(parrot_weight)
+
+parrot_weight = parrot_weight * 2
+
+print(parrot_weight)
+
