78e9f5accf2fa7f88f30bfa1f1faebc1d274ba46
Clement <clement@leadiq.com>
2022-11-30 17:24:33 +0800

    use short hand operators

--- part_one/types.py
+++ part_one/types.py
@@ -4,7 +4,19 @@
 tasty_seeds: int = 10
 
 # add 2 seeds to tasty_seeds
-tasty_seeds = tasty_seeds + 2
+tasty_seeds += 2
 
 # pass tasty_seeds into print function, to print it to console
+print(tasty_seeds)
+
+# minus 2 seeds!
+tasty_seeds -= 2
+print(tasty_seeds)
+
+# multiply the seeds by 4!
+tasty_seeds *= 4
+print(tasty_seeds)
+
+# divide the seeds by 2!
+tasty_seeds /= 2
 print(tasty_seeds)
